a788d2860d5ed256acbb3544799a62107d5ef836
MatteoZattini <matteozattini@hotmail.com>
2024-04-16 16:44:48 +0200

    added password script

---
+++ main.js
@@ -0,0 +1,9 @@
+document.writeln("La tua password è")
+
+let UserName = prompt ("inserisci il nome")
+
+let UserSurname = prompt ("Inserisci il tuo cognome")
+
+let UserColor = prompt ("inserisci il tuo colore preferito")
+
+document.writeln(UserName + UserSurname + UserColor + "#24")
